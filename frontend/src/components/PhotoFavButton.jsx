46622ef691173acf31b141a5f3df9764a3bad6b9
bahmed32 <bilqis.ali32@gmail.com>
2024-01-11 02:49:54 -0800

    added heart button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,16 +1,28 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+const PhotoFavButton = () => {
+  const { isActive, setIsActive } = useState(false);
+
+  const handleClick = () => {
+    console.log('Before:', isActive);
+    setIsActive(!isActive);
+    console.log('After:', isActive);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+    <div>
+      <FavIcon isActive={isActive} onClick={() => setsActive(!isActive)}
+        className={`icon ${isActive ? 'active' : ''}`} />
+      <div className="photo-list__fav-icon">
+        <div className="photo-list__fav-icon-svg">
+          {/* Insert React */}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default PhotoFavButton;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
 
@@ -9,6 +10,7 @@ const PhotoListItem = (props) => {
 
   return (
     <div className="photo-list__item">
+      <PhotoFavButton data={props.data} />
       <img className="photo-list__image" src={imageSource} alt={`Photo ${id}`} />
 
 
