adb8e8789b8fec182ffe47d415e09db4b10f95c7
bahmed32 <bilqis.ali32@gmail.com>
2024-01-11 18:38:21 -0800

    modified nav bar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TopicList from 'components/TopicList';
-import PhotoFavButton from './PhotoFavButton';
+import FavBadge from './FavBadge';
 
 import '../styles/TopNavigationBar.scss';
 
@@ -8,11 +8,13 @@ const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <div>
+
+      <div className="top-nav-bar__container">
+        <FavBadge />
         <TopicList />
-        <PhotoFavButton/>
       </div>
     </div>
+
   );
 };
 
